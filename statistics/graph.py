a90a59df38eae76e46daca0adfbbfc65a4ee2721
philippaspangenberg <philippa.spangenberg@arcor.de>
2020-08-31 16:20:46 +0200

    added vesselAnalysisPipeline.py with matlab skeletonization and added pruning to graph.py

--- statistics/graph.py
+++ statistics/graph.py
@@ -2,9 +2,10 @@ import networkx as nx
 from collections import defaultdict
 import time
 import statistics.filament as fil
-import skeleton.networkx_graph_from_array as netGraphArr
 from scipy import ndimage as ndi
+import numpy as np
 
+DIM = [2.0, 1.015625, 1.015625]
 
 class Graph:
     """
@@ -46,8 +47,8 @@ class Graph:
         Graph.degreeDict - A dictionary with the nth disjoint graph as the key containing a dictionary
                                 with key as the segment index (start node, end node) and value = segment branching angle
     """
-    def __init__(self, segmentation, skeleton):
-        self.networkxGraph = netGraphArr.get_networkx_graph_from_array(skeleton)
+    def __init__(self, segmentation, skeleton, networkxGraph):
+        self.networkxGraph = networkxGraph
         self.segmentsTotal = 0
         self.segmentsDict = defaultdict(dict)
         self.lengthDict = defaultdict(dict)
@@ -62,8 +63,10 @@ class Graph:
         self.degreeDict = defaultdict(dict)
         self.filaments = list(nx.connected_component_subgraphs(self.networkxGraph))
         self.compTime = 0
+
+        # calculate distance transform matrix
         self.initTime = time.time()
-        self.distTransf = ndi.distance_transform_edt(segmentation, sampling=[2.0, 1.015625, 1.015625])
+        self.distTransf = ndi.distance_transform_edt(segmentation, sampling=DIM)
         self.radiusMatrix = self.distTransf * skeleton
         print("time taken for distance transform is %0.3f seconds" % (time.time() - self.initTime))
 
@@ -93,22 +96,77 @@ class Graph:
                 filament = fil.Filament(adjacencyDict, start, self.radiusMatrix)
                 filament.dfs_iterative()
                 self.segmentsDict[ithDisjointGraph] = filament.segmentsDict
-                self.segmentsTotal = self.segmentsTotal + len(self.segmentsDict[ithDisjointGraph])
-                self.lengthDict[ithDisjointGraph] = filament.lengthDict
-                self.sumLengthDict[ithDisjointGraph] = sum(filament.lengthDict.values())
-                self.straightnessDict[ithDisjointGraph] = filament.straightnessDict
-                self.volumeDict[ithDisjointGraph] = filament.volumeDict
-                self.diameterDict[ithDisjointGraph] = filament.diameterDict
-                self.degreeDict[ithDisjointGraph] = filament.degreeDict
-                self.branchPointsDict[ithDisjointGraph] = filament.brPtsDict
-                self.endPointsDict[ithDisjointGraph] = filament.endPtsList
-                self.countBranchPointsDict[ithDisjointGraph] = len(filament.brPtsDict)
-                self.countEndPointsDict[ithDisjointGraph] = len(filament.endPtsList)
-                self.compTime += filament.compTime
+                # filament may have no segments left after postprocessing
+                if len(self.segmentsDict[ithDisjointGraph]) != 0:
+                    self.segmentsTotal = self.segmentsTotal + len(self.segmentsDict[ithDisjointGraph])
+                    self.lengthDict[ithDisjointGraph] = filament.lengthDict
+                    self.sumLengthDict[ithDisjointGraph] = sum(filament.lengthDict.values())
+                    self.straightnessDict[ithDisjointGraph] = filament.straightnessDict
+                    self.volumeDict[ithDisjointGraph] = filament.volumeDict
+                    self.diameterDict[ithDisjointGraph] = filament.diameterDict
+                    self.degreeDict[ithDisjointGraph] = filament.degreeDict
+                    self.branchPointsDict[ithDisjointGraph] = filament.brPtsDict
+                    self.endPointsDict[ithDisjointGraph] = filament.endPtsList
+                    self.countBranchPointsDict[ithDisjointGraph] = len(filament.brPtsDict)
+                    self.countEndPointsDict[ithDisjointGraph] = len(filament.endPtsList)
+                    self.compTime += filament.compTime
         print("filaments=", countDisjointGraphs)
         print("segments = ", self.segmentsTotal)
         print("summed time taken for dfs calculations is %0.3f seconds" % (self.compTime))
         print("time taken for statistic calculation is %0.3f seconds" % (time.time() - startTime))
 
+    def _getLength(self, path, dimensions=DIM):
+        """
+            Find length of a path as distance between nodes in it
 
+            Parameters
+            ----------
+            path : list
+                list of nodes in the path
 
+            dimensions : list
+                list with pixel dimensions in desired unit (e.g. microns)
+                3D: [z, y, x]   2D: [y, x]
+
+            Returns
+            -------
+            length : float
+                Length of path
+        """
+        length = 0
+        for index, item in enumerate(path):
+            if index + 1 != len(path):
+                item2 = path[index + 1]
+            vect = [j - i for i, j in zip(item, item2)]
+            vect = [a * b for a, b in zip(vect, dimensions)]    # multiply pixel length with original length
+            length += np.linalg.norm(vect)
+        return length
+
+    def _prune(self, scale=1.5):
+        startTime = time.time()
+        nodeDegreeDict = nx.degree(self.networkxGraph)
+        endPtsList = [k for (k, v) in nodeDegreeDict.items() if v == 1]
+        branchesToRemove = []
+        # find branch for all end points to its closest branch point
+        # if distance is below radius * scale => branch to remove
+        for endPt in endPtsList:
+            visited, stack = set(), [endPt]
+            branch = []
+            while stack:
+                vertex = stack.pop()
+                if vertex not in visited:
+                    visited.add(vertex)
+                    branch.append(vertex)
+                    neighbors = [n for n in self.networkxGraph.neighbors(vertex)]
+                    for neighbor in neighbors:
+                        if neighbor not in visited:
+                            stack.append(neighbor)
+                    if len(neighbors) > 2:  # branch point found
+                        if self._getLength(branch) <= self.radiusMatrix[vertex] * scale:
+                            branchesToRemove.append(branch)
+                        break
+        # delete branches from adjacency matrix besides branch points
+        for branch in branchesToRemove:
+            for node in branch[:-1]:
+                self.networkxGraph.remove_node(node)
+        print("pruned " + str(len(branchesToRemove)) + " branches in %0.3f seconds" % (time.time() - startTime))
---
+++ vesselAnalysisPipeline.py
@@ -0,0 +1,97 @@
+import os
+import numpy as np
+# NOTE This does the pyx compilation of this extension
+import pyximport; pyximport.install() # NOQA
+import skeleton.thinVolume as thinVol
+from skimage import io
+import utils
+import statistics.graph as graph
+from skimage.morphology import skeletonize_3d
+import time
+import matlab.engine
+#from memory_profiler import profile
+import skeleton.networkx_graph_from_array as netGraphArr
+
+def executeVesselAnalysisPipeline(imgPath, imgName, skeletonMethod='scikit', debug=False):
+    """
+        This is a pipeline containing the following steps for retrieving statistics from 3D vessel images:
+
+        1. ToDo: segment vessel image
+        2. generate skeleton
+        3. retrieve network graph
+        4. calculate statistics
+
+        Parameters
+        ----------
+        imgPath : string of image path
+        imgName : string of image name
+        skeletonMethod : string of skeletonization method
+        debug : bool
+            if debug, then plots are generated for each step
+    """
+
+    # read segmentation tif as numpy array
+    begin = time.time()
+    im = io.imread(imgPath + imgName + '.tif')              # reads image as array (z, y, x)
+    binArr = im / np.max(im)                                # make binary array of 3D image/ thresholded 3D volume
+    print("time taken to read segmentation tif is %0.3f seconds" % (time.time() - begin))
+
+    # 2) generate skeleton
+    if skeletonMethod != 'matlab':
+        if skeletonMethod == 'scikit':
+            start = time.time()
+            resultSkel = skeletonize_3d(binArr)
+            resultSkel = resultSkel / np.max(resultSkel)
+            print("time taken to calculate skeleton is %0.3f seconds" % (time.time() - start))
+        elif skeletonMethod == '3scan':
+            resultSkel = thinVol.get_thinned(binArr)  # get skeleton
+            resultSkel = resultSkel*1.0
+        print("time taken for complete skeletonization is %0.3f seconds" % (time.time() - begin))
+    else:
+        begin = time.time()
+        eng = matlab.engine.start_matlab()
+        print("time taken to start matlab engine is %0.3f seconds" % (time.time() - begin))
+        eng.cd(r'/Users/philippaspangenberg/Documents/ps-3scan-skeleton/skeleton3d-matlab-master', nargout=0)
+        eng.matlabSkeletonize(imgPath, imgName, nargout=0)
+        eng.quit()
+        # retrieve numpy array from skeleton tif
+        skel = io.imread(imgPath + 'skeleton-' + imgName + '.tif')  # reads image as array (z, y, x)
+        resultSkel = skel / np.max(skel)
+        print("time taken for complete skeletonization is %0.3f seconds" % (time.time() - begin))
+
+    # 3) extract graph
+    networkxGraph = netGraphArr.get_networkx_graph_from_array(resultSkel)
+
+    # 4) calculate statistics from segmentation and skeleton mask
+    statsSkel = graph.Graph(binArr, resultSkel, networkxGraph)
+    statsSkel.setStats()
+
+    # save statistics as CSV files in directory
+    statsDir = imgPath + '/Statistics_' + skeletonMethod + '/'
+    os.makedirs(os.path.dirname(statsDir), exist_ok=True)
+
+    utils.saveFilamentDictAsCSV(statsSkel.countBranchPointsDict, statsDir + imgName +
+                                '_Filament_No._Segment_Branch_Points.csv', 'Filament No. Segment Branch Pts')
+    utils.saveFilamentDictAsCSV(statsSkel.countEndPointsDict, statsDir + imgName +
+                                '_Filament_No._Segment_Terminal_Points.csv', 'Filament No. Segment Terminal Pts')
+    utils.saveFilamentDictAsCSV(statsSkel.sumLengthDict, statsDir + imgName + '_Filament_Length_(sum).csv',
+                                'Filament Length (sum)', 'um')
+    utils.saveSegmentDictAsCSV(statsSkel.lengthDict, statsDir + imgName + '_Segment_Length.csv', 'Segment Length', 'um')
+    utils.saveSegmentDictAsCSV(statsSkel.straightnessDict, statsDir + imgName + '_Segment_Straightness.csv',
+                               'Segment Straightness')
+    utils.saveSegmentDictAsCSV(statsSkel.degreeDict, statsDir + imgName + '_Segment_Branching_Angle.csv',
+                               'Segment Branching Angle', '°')
+    utils.saveSegmentDictAsCSV(statsSkel.volumeDict, statsDir + imgName + '_Segment_Volume.csv',
+                               'Segment Volume', 'um^3')
+    utils.saveSegmentDictAsCSV(statsSkel.diameterDict, statsDir + imgName + '_Segment_Diameter.csv',
+                               'Segment Diameter', 'um')
+    utils.saveSegmentDictAsCSV(statsSkel.branchPointsDict, statsDir + imgName + '_BranchPt_No._Branches.csv',
+                               'BranchPt No. Branches', category='Branch')
+    print("successfully saved statistics")
+
+    if debug:
+        utils.plot3DGrid(binArr, 'Segmentation Mask')  # plot 3D grid of binary image
+        utils.plot3DGrid(resultSkel, 'Skeleton Mask')  # plot 3D grid of skeleton
+        utils.plotSegStats(statsSkel.segmentsDict, statsSkel.branchPointsDict, statsSkel.endPointsDict)
+
+executeVesselAnalysisPipeline('/Users/philippaspangenberg/Desktop/synth_tubes/tube_3/', 'seg_tube_3_intensity_variation_noise', 'scikit')
